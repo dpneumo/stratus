8fcb5f04a1d75e6b13101cb67cf554038b89beae
Mitchell C Kuppinger <dpneumo@gmail.com>
2019-03-24 06:54:47 -0500

    postfix has it's own final steps. Building certs for nginx will be a separate set of scripts.

--- finalize/final_steps.sh
+++ finalize/final_steps.sh
@@ -1,36 +1,16 @@
 #!/usr/bin/env bash
+# Run:  ./final_steps.sh
+# will run in user's home dir
 
-cd /home/vagrant
+cd /home/$(whoami)
 
-printf "\n== Add google apppassword to sasl_password & restart postfic ==\n\n"
-# https://www.linode.com/docs/email/postfix/
-#           configure-postfix-to-send-mail-using-gmail-and-google-apps-on-debian-or-ubuntu/
-PFSASL='/etc/postfix/sasl'
-touch $PFSASL/sasl_password
-sudo cat <<-EOT >> $PFSASL/sasl_passwd
-  [smtp.gmail.com]:587 dpneumo@gmail.com:'$gapppass
-EOT
-sudo postmap    $PFSASL/sasl_passwd
-sudo chmod 0600 $PFSASL/sasl_passwd $PFSASL/sasl_passwd.db
-sudo systemctl restart postfix
-
-
-printf "\n========= Setup certs for nginx then restart it ========\n\n"
-ipaddr=$(ip route get 8.8.8.8 | awk '{print $7}')
-SUBJ=$subj SUBJ_IP=$ipaddr ./setup_ca.sh
-
-subjcert=${subj}_cert.pem
-
+printf "\n========= Place stratus server certs where cirrus can find them ========\n\n"
 mkdir -p cirrus/config/certs
-cp CA/cacert.pem  cirrus/config/certs/cacert.pem
-cp CA/certs/stratus_cert.pem  cirrus/config/certs/$subjcert
+cp 'CA/intermediate/certs/blacklakeca-chain.cert.pem'  'cirrus/config/certs/cacert.pem'
+cp 'CA/intermediate/certs/stratus.cert.pem'  'cirrus/config/certs/stratus.cert.pem'
 
+printf "\n========= Restart Nginx ===========================\n"
 sudo systemctl restart nginx
-
-printf "\n========= Show cirrus/config/certs/cacert.pem ==========\n\n"
-cat cirrus/config/certs/cacert.pem
-printf "\n========= Show cirrus/config/certs/$subjcert ===========\n\n"
-cat cirrus/config/certs/$subjcert
 
 printf "\n========= Start Rails App =========================\n"
 cd cirrus
