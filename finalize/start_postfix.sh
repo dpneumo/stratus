d4f557994ca007dd8e2ab77f18313703397bad27
Mitchell C Kuppinger <dpneumo@gmail.com>
2019-03-24 06:48:49 -0500

    Script to finish postfix setup and start it

---
+++ finalize/start_postfix.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+printf "\n== Add google apppassword to sasl_password & restart postfic ==\n\n"
+# https://www.linode.com/docs/email/postfix/
+#           configure-postfix-to-send-mail-using-gmail-and-google-apps-on-debian-or-ubuntu/
+PFSASL='/etc/postfix/sasl'
+touch $PFSASL/sasl_password
+sudo cat <<-EOT >> $PFSASL/sasl_passwd
+  [smtp.gmail.com]:587 dpneumo@gmail.com:'$gapppass
+EOT
+sudo postmap    $PFSASL/sasl_passwd
+sudo chmod 0600 $PFSASL/sasl_passwd $PFSASL/sasl_passwd.db
+sudo systemctl restart postfix
