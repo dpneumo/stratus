fea9b1baeb0e14d52d55142a09f1a11c88cb7486
Mitchell C Kuppinger <dpneumo@gmail.com>
2019-11-29 23:13:57 -0600

    setting the sasl access to gmail - does not like leading spaces

--- finalize/start_postfix.sh
+++ finalize/start_postfix.sh
@@ -6,7 +6,7 @@ printf "\n== Add google apppassword to sasl_password & restart postfic ==\n\n"
 PFSASL='/etc/postfix/sasl'
 touch $PFSASL/sasl_password
 sudo cat <<-EOT >> $PFSASL/sasl_passwd
-  [smtp.gmail.com]:587 dpneumo@gmail.com:'$gapppass
+[smtp.gmail.com]:587 dpneumo@gmail.com:'$gapppass
 EOT
 sudo postmap    $PFSASL/sasl_passwd
 sudo chmod 0600 $PFSASL/sasl_passwd $PFSASL/sasl_passwd.db
