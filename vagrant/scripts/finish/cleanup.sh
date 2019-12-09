d8b6178b3da35a04820933445a7f2061ed1021e5
Mitchell C Kuppinger <dpneumo@gmail.com>
2019-12-08 20:40:27 -0600

    Add sasl_passwd to the files to be cleaned.

--- vagrant/scripts/finish/cleanup.sh
+++ vagrant/scripts/finish/cleanup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 printf "\n========= Remove duplicate entries from files =====\n"
-for f in ~/.bash_profile ~/.gemrc; do
+for f in ~/.bash_profile ~/.gemrc /etc/postfix/sasl/sasl_passwd; do
   cp $f $f.bkup
   awk '{if (++dup[$0] == 1) print $0;}' $f > ~/tmp
   cat ~/tmp > $f && rm $f.bkup
