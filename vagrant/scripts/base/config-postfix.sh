e5c5bcdf773c286cafef3edf86c21f4bc786b54d
Mitchell C Kuppinger <dpneumo@gmail.com>
2019-12-25 23:26:10 -0600

    Personalize postfix main.cf once in place

--- vagrant/scripts/base/config-postfix.sh
+++ vagrant/scripts/base/config-postfix.sh
@@ -5,10 +5,15 @@ SRC='/vagrant/client_files'
 printf "\n========= Setup Postfix ===========================\n"
 yum install cyrus-sasl-plain -y
 mkdir /etc/postfix/sasl
+
 mv /etc/aliases           /etc/aliases.$(date +%s)
 mv /etc/postfix/main.cf   /etc/postfix/main.cf.$(date +%s)
 cp $SRC/postfix/aliases   /etc/
 cp $SRC/postfix/main.cf   /etc/postfix/
 chmod 644 /etc/aliases*   /etc/postfix/main.cf*
+
+sed -i "s|myhost.mydomain.com|$PFixHostName|g" \
+  /etc/postfix/main.cf
+
 newaliases
 # Start postfix in final_steps.sh
