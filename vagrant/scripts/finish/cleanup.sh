fd6145900f39ec75d7f9d4b55712e4e29b2ce13b
Mitchell C Kuppinger <dpneumo@gmail.com>
2019-12-27 00:08:06 -0600

    Cleanup .bashrc for root and vagrant users by removing the env var exports

--- vagrant/scripts/finish/cleanup.sh
+++ vagrant/scripts/finish/cleanup.sh
@@ -6,3 +6,13 @@ for f in ~/.bash_profile ~/.gemrc /etc/postfix/sasl/sasl_passwd; do
   awk '{if (++dup[$0] == 1) print $0;}' $f > ~/tmp
   cat ~/tmp > $f && rm $f.bkup && rm ~/tmp
 done
+
+printf "\n========= Cleanup .bashrc for root and vagrant ====\n"
+sed -i '/^# Start Setup Vars/,/^# End Setup Vars/d' ~/.bashrc
+sudo sed -i '/^# Start Setup Vars/,/^# End Setup Vars/d' ~/.bashrc
+
+printf "\n========= Cleanup home dir ========================\n"
+if [[ ! -e ~/bkup ]]; then
+  mkdir ~/bkup
+fi
+mv  *.sh.* ~/bkup/
