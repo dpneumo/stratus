97be8de0af7c0b89d77f0babb626e81998d1e30f
Mitchell C Kuppinger <dpneumo@gmail.com>
2019-12-27 00:06:43 -0600

    Move home dir cleanup to cleanup script.

--- vagrant/scripts/finish/finalize-setup.sh
+++ vagrant/scripts/finish/finalize-setup.sh
@@ -7,9 +7,3 @@ cp $FIN/start_postfix.sh start_postfix.sh -fb --suffix=.$(date +%s)
 
 printf "\n========= Insure home folder scripts are runable ==\n"
 sudo chmod 755 *ca.sh stratus_server_cert.sh
-
-# Cleanup home dir
-if [[ ! -e ~/bkup ]]; then
-  mkdir ~/bkup
-fi
-mv  *.sh.* ~/bkup/
