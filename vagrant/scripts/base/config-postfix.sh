2b1da86202c64fe7cdeb081420d61e41938a745b
Mitchell C Kuppinger <dpneumo@gmail.com>
2020-01-13 22:43:53 -0600

    Do not repeat work already done. Reorganize code a little.

--- vagrant/scripts/base/config-postfix.sh
+++ vagrant/scripts/base/config-postfix.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 SRC='/vagrant/client_files'
+PFSASL='/etc/postfix/sasl'
 
 # Run as root
 printf "\n========= Setup Postfix ===========================\n"
 yum install cyrus-sasl-plain -y
-mkdir /etc/postfix/sasl
 
 mv /etc/aliases           /etc/aliases.$(date +%s)
 mv /etc/postfix/main.cf   /etc/postfix/main.cf.$(date +%s)
@@ -20,14 +20,18 @@ newaliases
 printf "\n== Add google apppassword to sasl_password & restart postfix ==\n\n"
 # https://www.linode.com/docs/email/postfix/
 #    configure-postfix-to-send-mail-using-gmail-and-google-apps-on-debian-or-ubuntu/
-PFSASL='/etc/postfix/sasl'
+if [ ! -e $PFSASL ]; then mkdir $PFSASL; fi
 
-if ! [ -e "$PFSASL/sasl_passwd" ]; then touch $PFSASL/sasl_password; fi
+if [ ! -e $PFSASL/sasl_passwd ]
+then
+  touch $PFSASL/sasl_password
+else
+  sed -i '/dpneumo@gmail.com:/d' $PFSASL/sasl_passwd
+fi
 
 sudo cat <<-EOT >> $PFSASL/sasl_passwd
 '$PFixGmail'
 EOT
-sudo cat $PFSASL/sasl_passwd
 
 sudo postmap    $PFSASL/sasl_passwd
 sudo chmod 0600 $PFSASL/sasl_passwd $PFSASL/sasl_passwd.db
